e84f9c7eca17252a3cc0a86e9e32f548051a3a6b
Jonathan <jonajvdm@gmail.com>
2020-07-15 13:12:26 +0200

    Added logout to the sidebar for mobile screens

--- src/dashboard/components/Sidebar.tsx
+++ src/dashboard/components/Sidebar.tsx
@@ -1,18 +1,39 @@
+import { Btn, Color } from '../../components/components';
+import { connect } from 'react-redux';
+import { logout } from '../../redux/actions';
 import React from 'react';
 
 interface Props {
-    expanded: boolean
+  expanded: boolean,
+  logout: any
 }
 
-export default function Sidebar({expanded}: Props): JSX.Element {
-    function className() {
-        if (expanded) return 'sidebar sidebar--expanded';
-        return 'sidebar';
-    }
+function Sidebar({ expanded, logout }: Props): JSX.Element {
+  function className() {
+    if (expanded) return 'sidebar sidebar--expanded';
+    return 'sidebar';
+  }
 
-    return (
-        <div className={className()}>
-            Sidebar
-        </div>
-    );
+  function signOut() {
+    logout();
+  }
+
+  return (
+    <div className={className()}>
+      Sidebar
+
+      <div className="sidebar__logout mar-top">
+        <Btn text="Logout" color={Color.Danger} onClick={signOut} />
+      </div>
+    </div>
+  );
 }
+
+function mapDispatch(dispatch: any) {
+  return {
+    logout: async () =>
+      await dispatch(logout()),
+  }
+}
+
+export default connect(null, mapDispatch)(Sidebar);
