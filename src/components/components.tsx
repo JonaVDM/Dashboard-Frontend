66334bd443ff6ee3321877776182d47f4705b2d8
Jonathan <jonajvdm@gmail.com>
2020-07-23 17:49:29 +0200

    Added a reusble table for desktop sizes

--- src/components/components.tsx
+++ src/components/components.tsx
@@ -1,3 +1,4 @@
 export * from './text-field/TextField';
 export * from './btn/Btn';
 export * from './card/Card';
+export * from './table-list/TableList';
--- src/users-manager/UsersManager.tsx
+++ src/users-manager/UsersManager.tsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Dashboard from '../dashboard/Dashbaord';
 import { Card, Sizes } from '../components/components';
+import { connect } from 'react-redux';
+import { RootState } from '../redux/reducers';
+import UserList from './UserList';
 
-function UsersManager(): JSX.Element {
+interface Props {
+  token: string,
+}
+
+function UsersManager({token}: Props): JSX.Element {
+  let [users, setUsers] = useState<User[]>([]);
+  let [message, setMessage] = useState<string>();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {loadUsers()}, []);
+
+  async function loadUsers() {
+    let request = await fetch('/api/user', {
+      headers: {
+        'x-token': token,
+      }
+    });
+
+    let data = await request.json();
+    if (data.users) {
+      setUsers(data.users);
+    } else {
+      setMessage(data.message);
+    }
+  }
+
   return (
     <Dashboard>
       <div className="grid">
+        {message}
+
         <Card size={Sizes.full}>
-          <h1 className="h1">User Manger</h1>
+          <p className="h1">User Manger</p>
+        </Card>
+
+        <Card>
+          <UserList users={users} />
         </Card>
       </div>
     </Dashboard>
   );
 }
 
-export default UsersManager;
+function mapState(state: RootState) {
+  return {
+    token: state.auth.token
+  }
+};
+
+function mapDispatch(dispatch: any) {
+  return {}
+}
+
+export default connect(mapState, mapDispatch)(UsersManager);
